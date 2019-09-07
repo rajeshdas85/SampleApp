8ea3cf5902bb61de3fde4eff4763174424153561
Rajesh Das <rajesh.das85@live.com>
2019-09-07 14:34:07 +0530

    Commited with New task

--- test/SimpleAPI.Tests/UnitTest1.cs
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -1,14 +1,24 @@
 using System;
 using Xunit;
+using  SimpleAPI.Controllers;
 
 namespace SimpleAPI.Tests
 {
     public class UnitTest1
     {
+        ValuesController controller= new ValuesController();
         [Fact]
         public void Test1()
         {
 
         }
+
+     ValuesController controller = new ValuesController();
+[Fact]
+public void GetReturnsCorrectNumber()
+{
+  var returnValue = controller.Get(1);
+  Assert.Equal("Rajesh Das", returnValue.Value);
+}
     }
 }
